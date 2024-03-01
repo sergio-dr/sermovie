3d6fd9c7a507887f6d6877bed1023016dc25eab2
Sergio <sdiaz@us.es>
2024-03-01 21:27:31 +0100

    Revised packaging. Version string is managed on .toml file by now

--- src/sermovie/sermovie.py
+++ src/sermovie/sermovie.py
@@ -18,8 +18,6 @@ You should have received a copy of the GNU General Public License along with
 this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = "0.5"
-
 import numpy as np
 from datetime import datetime
 import warnings
